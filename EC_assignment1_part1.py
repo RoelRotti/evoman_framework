d4945fd049e9a4d3bc72da19a3f473b4e4a86c00
Yoes Ywema <y.ywema@student.vu.nl>
2021-09-21 13:39:29 +0200

    met manier om door generaties heen te itereren

--- EC_assignment1_part1.py
+++ EC_assignment1_part1.py
@@ -59,19 +59,33 @@ def run(config_path):
                                 neat.DefaultStagnation, config_path)
 
     # builds the population of genomes
-    p = neat.Population(config)
+    p1 = neat.Population(config)
+    p2 = neat.Population(config)
+    p3 = neat.Population(config)
+    p4 = neat.Population(config)
 
     # statistics during run
-    p.add_reporter(neat.StdOutReporter(True))
-    p.add_reporter(neat.StatisticsReporter())
+    p1.add_reporter(neat.StdOutReporter(True))
+    p1.add_reporter(neat.StatisticsReporter())
+    p2.add_reporter(neat.StdOutReporter(True))
+    p2.add_reporter(neat.StatisticsReporter())
+    p3.add_reporter(neat.StdOutReporter(True))
+    p3.add_reporter(neat.StatisticsReporter())
+    p4.add_reporter(neat.StdOutReporter(True))
+    p4.add_reporter(neat.StatisticsReporter())
 
     # the amount of generations it is run for
     amount_generations = 100
     # winner =  winning genomes
-    winner = p.run(fitness_function=eval_genomes, n=amount_generations)
-
-
-
+    for i in range(amount_generations):
+        winner_p1 = p1.run(fitness_function=eval_genomes, n=1)
+        pop1 = p1.population.values()
+        winner_p2 = p2.run(fitness_function=eval_genomes, n=1)
+        pop2 = p1.population.values()
+        winner_p3 = p3.run(fitness_function=eval_genomes, n=1)
+        pop3 = p1.population.values()
+        winner_p4 = p4.run(fitness_function=eval_genomes, n=1)
+        pop4 = p1.population.values()
 
 if __name__ == "__main__":
     # gives us the path to the directory we are in 
