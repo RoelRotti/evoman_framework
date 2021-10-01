57df5fc123e9bf08d8bc96422d8da8f21ffba555
Yoes Ywema <y.ywema@student.vu.nl>
2021-10-01 08:52:27 +0200

    eemy 2 with optimized hyperparams

--- EC_assignment1_part1.py
+++ EC_assignment1_part1.py
@@ -27,7 +27,7 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 # if headless = True : Will not run visuals (hence be faster)
-headless = False
+headless = True
 if headless:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
@@ -88,7 +88,7 @@ def migration(populations, n_migrations):
 
 # To specify how many islands to use
 
-number_of_islands = 1
+number_of_islands = 4
 number_of_runs = 10
 
 def run(config_path):#, df, n_run):
